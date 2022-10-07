fb4c399be5a4c78388f5e112acc8c8cb6acd3541
AESiR <cyborgkiller1008@gmail.com>
2022-10-08 02:51:23 +0530

    flask integration

--- baap.py
+++ baap.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template, request
+from operator import index
+from traceback import print_tb
+from unittest import result
+from winreg import REG_DWORD
+from flask import Flask, render_template, request, url_for, flash, redirect
+from matplotlib.style import use
+from sqlalchemy import true
 app = Flask(__name__)
-result={'resp':True}
+
 
 @app.route('/',methods = ['POST', 'GET'])
-def result():
+def home():
+   if request.method == 'POST':
+      return redirect(url_for("login"))
    return render_template("home.html")
+
+@app.route('/login', methods = ['POST', 'GET'] )
+def login():
+   if request.method== 'POST':
+      username=request.form["username"]
+      print(username)
+      try:
+         return redirect(url_for("feedback"))
+      except Exception as e:
+         return redirect(url_for("login"))
+   
+   
+   return render_template("login.html")
+@app.route('/feedback')
+def feedback():
+   return render_template("feedbackform.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
